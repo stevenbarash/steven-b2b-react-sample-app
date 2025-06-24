46d314770d9ee857561f97247fe5229d141a507a
Steven Barash <steven.barash@descope.com>
2025-06-24 08:07:04 -0400

    add themes

--- src/auth/signIn/SignIn.js
+++ src/auth/signIn/SignIn.js
@@ -66,7 +66,7 @@ const SignIn = () => {
                   className="style-dropdown"
                 >
                   <option value="">Default Style</option>
-                  <option value="ntpluslavoro">NT Plus Lavoro Style</option>
+                  <option value="tylenol">Tylenol</option>
                 </select>
               </div>
               
